df655411a39edba17c392ef24c60a7880d6f6b0d
Yevhen Horbachov <killabee1985@gmail.com>
2019-02-13 15:56:20 +0100

    add responsiveness

--- src/app/blocks/blocks-list/blocks-list.component.ts
+++ src/app/blocks/blocks-list/blocks-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { LtoPublicNodeService, BlockHeader } from '@app/core';
-import { switchMap, exhaustMap, shareReplay, take, startWith, scan } from 'rxjs/operators';
+import { LtoPublicNodeService, BlockHeader, ScreenService, ScreenSize } from '@app/core';
+import { switchMap, exhaustMap, shareReplay, take, startWith, scan, map } from 'rxjs/operators';
 import { Subject, merge, Observable, of } from 'rxjs';
 
 @Component({
@@ -12,9 +12,27 @@ export class BlocksListComponent implements OnInit {
   loadMore$ = new Subject<any>();
   height$ = new Subject<number>();
   blocks$: Observable<BlockHeader[]>;
-  visibleColumns$: Observable<string[]> = of(['height', 'generator', 'size', 'transactionsCount']);
 
-  constructor(private _publicNode: LtoPublicNodeService) {
+  get generatorLength(): number {
+    switch (this._screen.size) {
+      case ScreenSize.XS:
+      case ScreenSize.SM:
+        return 8;
+      case ScreenSize.MD:
+      case ScreenSize.LG:
+        return 18;
+      default:
+        return 40;
+    }
+  }
+
+  get visibleColumns(): string[] {
+    return this._screen.size <= ScreenSize.SM
+      ? ['height', 'generator', 'transactionsCount']
+      : ['height', 'generator', 'size', 'transactionsCount'];
+  }
+
+  constructor(private _publicNode: LtoPublicNodeService, private _screen: ScreenService) {
     const height$ = merge(_publicNode.blocksHeight(), this.height$).pipe(shareReplay(1));
 
     this.blocks$ = this.loadMore$.pipe(
