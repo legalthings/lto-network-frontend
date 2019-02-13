91f33b9011f051ec5754b6ea6074fe07365954ae
Yevhen Horbachov <killabee1985@gmail.com>
2019-02-13 17:08:42 +0100

    add home link

--- src/app/core/components/appbar/appbar.module.ts
+++ src/app/core/components/appbar/appbar.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { MatToolbarModule } from '@angular/material';
 import { AppbarComponent } from './appbar.component';
 
 @NgModule({
   declarations: [AppbarComponent],
-  imports: [CommonModule, MatToolbarModule],
+  imports: [CommonModule, MatToolbarModule, RouterModule.forChild([])],
   exports: [AppbarComponent]
 })
 export class AppbarModule {}
