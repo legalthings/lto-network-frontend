355274780ffbe2334db2701cc3a4ba95fffbdf92
Yevhen Horbachov <killabee1985@gmail.com>
2019-03-05 13:20:35 +0100

    add create/import account pages

--- apps/wallet/src/app/auth/import-account/import-account.component.ts
+++ apps/wallet/src/app/auth/import-account/import-account.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LocalAccountsService, AuthService } from '../../core';
+import { MatSnackBar } from '@angular/material';
+import { LtoService } from '@lto/core';
 
 @Component({
   selector: 'wallet-import-account',
@@ -6,10 +11,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./import-account.component.scss']
 })
 export class ImportAccountComponent implements OnInit {
+  importForm!: FormGroup;
 
-  constructor() { }
+  constructor(
+    private _localAccountsService: LocalAccountsService,
+    private _authService: AuthService,
+    private _ltoApi: LtoService,
+    private _router: Router,
+    private _snackbar: MatSnackBar
+  ) {}
 
   ngOnInit() {
+    this.importForm = new FormGroup({
+      seedPhrase: new FormControl('', [Validators.required]),
+      accountName: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required])
+    });
   }
 
+  importAccountAndRedirect() {
+    if (this.importForm.invalid) {
+      return;
+    }
+    try {
+      const { seedPhrase, accountName, password } = this.importForm.value;
+
+      const ltoAccount = this._ltoApi.createAccountFromExistingPhrase(seedPhrase);
+      const userAccount = this._localAccountsService.createLocalAccount(
+        ltoAccount,
+        accountName,
+        password
+      );
+      this._authService.login(userAccount, password);
+      this._snackbar.open('Account imported', 'Dismiss');
+      this._router.navigate(['/', 'transfers']);
+    } catch (error) {
+      this._snackbar.open('Unable to import account', 'Dismiss');
+      console.error(error);
+    }
+  }
 }
