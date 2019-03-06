585283121617642c99164f3c1393899d631dca3e
Yevhen Horbachov <zpirit@iPad-van-Tobit.localdomain>
2019-03-06 10:58:00 +0100

    add import-account tests

--- apps/wallet/src/app/auth/import-account/import-account.component.spec.ts
+++ apps/wallet/src/app/auth/import-account/import-account.component.spec.ts
@@ -1,34 +1,50 @@
 import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { ImportAccountComponent } from './import-account.component';
+import { Router } from '@angular/router';
 import { By } from '@angular/platform-browser';
 import { SharedModule } from '../../shared';
 import { PasswordFormFieldModule } from '../components/password-form-field/password-form-field.module';
 import { LocalAccountsService, AuthService } from '../../core';
+import { LtoService } from '@lto/core';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { of } from 'rxjs';
 
-fdescribe('auth/ImportAccountComponent', () => {
+describe('auth/ImportAccountComponent', () => {
   let component: ImportAccountComponent;
   let fixture: ComponentFixture<ImportAccountComponent>;
   let localAccounts: LocalAccountsService;
   let authService: AuthService;
+  let ltoService: LtoService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [SharedModule, PasswordFormFieldModule],
+      imports: [SharedModule, PasswordFormFieldModule, NoopAnimationsModule],
       declarations: [ImportAccountComponent],
       providers: [
         {
           provide: LocalAccountsService,
-          useValue: {} as Partial<LocalAccountsService>
+          useValue: {
+            availableAccounts$: of([])
+          } as Partial<LocalAccountsService>
         },
         {
           provide: AuthService,
           useValue: {} as Partial<AuthService>
+        },
+        {
+          provide: LtoService,
+          useValue: {} as Partial<LtoService>
+        },
+        {
+          provide: Router,
+          useValue: {}
         }
       ]
     }).compileComponents();
 
     localAccounts = TestBed.get(LocalAccountsService);
     authService = TestBed.get(AuthService);
+    ltoService = TestBed.get(LtoService);
 
     fixture = TestBed.createComponent(ImportAccountComponent);
     component = fixture.componentInstance;
@@ -37,5 +53,6 @@ fdescribe('auth/ImportAccountComponent', () => {
 
   it('should disable Login button if no local accounts presented', () => {
     const loginLink = fixture.debugElement.query(By.css('.login-link'));
+    expect(loginLink.attributes.disabled).toBe('true');
   });
 });
